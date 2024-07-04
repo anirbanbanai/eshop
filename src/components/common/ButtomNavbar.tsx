421c23058abbec6d9704a7d38423cfcc8f63a6c8
Anirban Banai <anirbanchondrobanai@gmail.com>
2024-07-04 17:38:56 +0600

    up

--- src/components/common/ButtomNavbar.tsx
+++ src/components/common/ButtomNavbar.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { BiSolidCartDownload } from "react-icons/bi";
 import { BsChatDotsFill } from "react-icons/bs";
 import { CiHome, CiGrid42 } from "react-icons/ci";
+import { FaAmericanSignLanguageInterpreting } from "react-icons/fa";
 import { GiSkills } from "react-icons/gi";
 
 const ButtomNavbar = () => {
@@ -9,26 +10,29 @@ const ButtomNavbar = () => {
     <div className="bg-slate-200 py-4 px-4 fixed bottom-0 z-50 w-full  bg-glassmorphism  backdrop-blur-lg xs:px-7 md:hidden ">
       <ul className="overflow-x-auto flex gap-2 justify-between items-center">
         <Link className="flex gap-1 items-center" href={"/"}>
-          <CiHome className="text-xl text-orange-600 " />
+        <FaAmericanSignLanguageInterpreting className="text-5xl text-pink-500 " />
+        </Link>
+        <Link className="flex gap-1 items-center" href={"/"}>
+          <CiHome className="text-2xl text-orange-600 " />
           <p className="max-lg:hidden text-sm font-semibold">Home</p>
         </Link>
 
         <Link className="flex gap-1 items-center" href={"/dashboard"}>
-          <CiGrid42 className="text-xl text-orange-600 " />
+          <CiGrid42 className="text-2xl text-orange-600 " />
           <p className="max-lg:hidden text-sm font-semibold">Dashboard</p>
         </Link>
 
         <Link className="flex gap-1 items-center" href={"/chat"}>
-          <BsChatDotsFill className="text-xl text-orange-600 " />
+          <BsChatDotsFill className="text-2xl text-orange-600 " />
           <p className="max-lg:hidden text-sm font-semibold">Chat</p>
         </Link>
         <Link className="flex gap-1 items-center" href={"/cart"}>
-          <BiSolidCartDownload className="text-xl text-orange-600 " />
+          <BiSolidCartDownload className="text-2xl text-orange-600 " />
           <p className="max-lg:hidden text-sm font-semibold">Cart</p>
         </Link>
 
         <Link className="flex gap-1 items-center" href={"/"}>
-          <GiSkills className="text-xl text-orange-600 " />
+          <GiSkills className="text-2xl text-orange-600 " />
           <p className="max-lg:hidden text-sm font-semibold">About-Us</p>
         </Link>
       </ul>
